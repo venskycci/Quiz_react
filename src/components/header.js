84fc5708c659cc19a67aa50f81d3e73383bb7e23
venskycci <95100725+venskycci@users.noreply.github.com>
2024-08-18 00:02:58 -0300

    Update header.js
    update anto

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import '../index.css';
 
 function Header(props){
         return (<div id="cabeza">
-                <section id="country" translate='no'>COUNTRY QUIZ</section>
+                <section id="country" translate='no'>antonella QUIZ</section>
                 { props.value && <aside id="aventura"> <img src={header} alt='' height="145" width="160"/> </aside>}
             </div>
         )
